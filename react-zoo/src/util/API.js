2100591ff188343f9b41cdadcdffa9d24f68d73b
brandongatlin <brandongatlin1981@me.com>
2019-07-20 20:51:38 -0500

    resized images in grid

--- react-zoo/src/util/API.js
+++ react-zoo/src/util/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
   search: function(query) {
     return axios.get(
-      `https://api.giphy.com/v1/gifs/search?api_key=garZqKF43Z1oYqEuQRR2Nr300rHn2n9r&q=${query}&limit=12&offset=0&rating=G&lang=en`
+      `https://api.giphy.com/v1/gifs/search?api_key=garZqKF43Z1oYqEuQRR2Nr300rHn2n9r&q=${query}&limit=10&offset=0&rating=G&lang=en`
     );
   }
 };
